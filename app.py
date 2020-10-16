191fcb4f50d965d8beb7300f9142eb40951bd654
Chad Heisler <chadheisler5@gmail.com>
2020-10-16 16:25:54 -0700

    adding routes for adding pet

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, redirect
 from models import db, connect_db, Pet
 from flask_debugtoolbar import DebugToolbarExtension
 
+from forms import AddPetForm
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'BOOO!'
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
@@ -19,3 +21,24 @@ def home():
     pets = Pet.query.all()
 
     return render_template("home.html", pets=pets)
+
+
+@app.route("/add_pet", methods=["GET", "POST"])
+def add_pet():
+
+    
+
+    form = AddPetForm()
+
+    if form.validate_on_submit():
+        pet.name = form.name.data
+        pet.species = form.species.data
+        pet.photo_url = form.photo.data
+        pet.age = form.age.data
+        pet.notes = form.note.data
+        db.session.commit()
+
+        return redirect("/")
+
+    else:
+        return render_template("/add_pet.html", form=form)
--- forms.py
+++ forms.py
@@ -1,3 +1,25 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, FloatField
+"""Forms for our demo Flask app."""
 
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, TextAreaField, TextArea
+from wtforms.validators import InputRequired, Optional, Email
+
+
+class AddPetForm(FlaskForm):
+    """Form for adding snacks."""
+
+    name = StringField("Pet name:")
+    species = StringField("Species:")
+    photo = StringField("Photo URL:")
+    age = SelectField('Age:',  choices=[('baby', 'Baby'), ('young', 'Young'), 
+                                        ('adult', 'Adult'), ('senior', 'Senior'), ('dead', 'Dead')])
+    note = TextAreaField('Notes:', widget=TextArea(row=30, cols=10))
+
+
+class UserForm(FlaskForm):
+    """Form for adding/editing friend."""
+
+    name = StringField("Name",
+                       validators=[InputRequired()])
+    email = StringField("Email Address",
+                        validators=[Optional(), Email()])
